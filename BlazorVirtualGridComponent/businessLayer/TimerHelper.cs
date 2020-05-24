84cded60d9526591d65b6d0199d5d3fa9c418046
Lupusa87 <VakhtangiAbashidze@gmail.com>
2020-05-23 21:33:03 -0400

    update to RTM

--- BlazorVirtualGridComponent/businessLayer/TimerHelper.cs
+++ BlazorVirtualGridComponent/businessLayer/TimerHelper.cs
@@ -20,23 +20,20 @@ namespace BlazorVirtualGridComponent.businessLayer
             {
                 Stop();
             }
-            else
+
+            IsRunning = true;
+
+            if (DueTime < 1)
             {
-                IsRunning = true;
-
-                if (DueTime<1)
-                {
-                    DueTime = 1; //It can be 0 but in case of 0 blazor does not update
-                }
-
-                if (Period < 1)
-                {
-                    Period = 1;
-                }
-
-                timer1 = new Timer(Timer1Callback, null, DueTime, Period);
-                
+                DueTime = 1; //It can be 0 but in case of 0 blazor does not update
             }
+
+            if (Period < 1)
+            {
+                Period = 1;
+            }
+
+            timer1 = new Timer(Timer1Callback, null, DueTime, Period);
 
         }
 
